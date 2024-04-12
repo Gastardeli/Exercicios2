b7f568a5000aa1bf8e463db5ce7d7a7f0dfaca03
Victor <133364840+Gastardeli@users.noreply.github.com>
2024-04-12 16:31:26 -0300

    Add files via upload

---
+++ qualquernome/qualquernome/qualquernome.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	/*int idade = 18;
+	if (idade >= 18) {
+		cout << "Pode tirar habilitação";
+	}
+	else {
+		cout << "Não pode";
+	}*/
+
+	/*int altura = 121;
+	if (altura <= 155) {
+		cout << "Não pode pode ir no brinquedo";
+
+	} else if (altura >= 210) {
+		cout << "Não pode ir no brinquedo";
+	}
+	else {
+		cout << "Pode ir no brinquedo";
+	}
+
+	return 0;*/
+
+
+	//EXERCICIO 1
+
+	/*int quantidade = 26;
+	if (quantidade > 25) {
+		float calculo = quantidade * 1.25;
+		cout << calculo;
+	}else if(quantidade <= 25) {
+		float calculo = quantidade * 1.50;
+		cout << calculo;
+		}*/
+
+
+	//EXERCICIO 2
+
+	/*int numero = 5;
+	if (numero > 10) {
+		int calculo= numero + 10 * 2.;
+		cout << calculo;
+	}
+	else {
+		int calculo = numero + 9 * 3 - 8;
+		cout << calculo;
+	}*/
+
+
+	//EXERCICIO 3
+
+	/*int numero = 2;
+	if (numero >= 0) {
+		cout << "POSITIVO";
+	}
+	else {
+		cout << "NEGATIVO";
+	}*/
+
+
+	//EXERCICIO 4
+
+	/*int variavel1 = 1;
+	int variavel2 = 2;
+	if (variavel1 > variavel2) {
+		cout << "A primeira é maior";
+	}
+	else {
+		cout << "A segunda é maior";
+	}*/
+
+
+	//EXERCICIO 5
+
+	/*int nota1 = 5;
+	int nota2 = 10;
+
+	int calculo = nota1 + nota2;
+	int nsei = calculo / 2;
+
+	if (nsei>=7) {
+		cout << "APROVADO";
+	}
+	else {
+		cout << "REPROVADO";
+	}*/
+
+
+	return 0;
+
+}
